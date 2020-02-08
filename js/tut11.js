6b74c8d01f54f66fe753443cea26e780a074ae56
montu patel <bhushanketanbhai.patel@calbaptist.edu>
2020-02-07 20:28:24 -0800

    2/7/2020

---
+++ js/tut11.js
@@ -0,0 +1,22 @@
+// DOM (document object model...)
+
+// Document object is part of window object
+
+// window is globle scope..
+
+let a = window;
+
+//window.alert('Hello Montu');
+//a = prompt('This is not good');
+
+//a = confirm('are you sure you want to kill your self');
+
+a= window.innerWidth;
+a= innerHeight;
+a =scrollY;
+a= location.toString();
+a= window.history;
+
+
+
+console.log(a);
