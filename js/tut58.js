61e5a387918ccf2522129e8fd656f5ba7452a658
montu patel <bhushanketanbhai.patel@calbaptist.edu>
2020-02-19 02:58:56 -0800

    2/19/2020

---
+++ js/tut58.js
@@ -0,0 +1,28 @@
+console.log('This is tutorial 58');
+
+//This is javascript from web development course
+
+function greet(name, byeText) {
+    console.log('good Morning ' + name + " " + byeText);
+
+}
+//greet('Harry');
+timeOut = setTimeout(greet, 5000, "Montu", "Take Care"); // in syntex you cannot call function inside
+console.log(timeOut);
+clearTimeout(timeOut);
+
+// Set interval
+
+intervalId = setInterval(greet, 2000, "montu", "Good night");
+clearInterval(intervalId);
+
+// display time 
+
+function displayTime() {
+    time = new Date();
+    console.log(time);
+    document.getElementById('time').innerHTML =time;
+    
+}
+
+setInterval(displayTime, 1000)
