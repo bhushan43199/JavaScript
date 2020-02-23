08f316e97763df93d72f80b8f18e13a3f79e9b3e
montu patel <bhushanketanbhai.patel@calbaptist.edu>
2020-02-22 20:16:26 -0800

    2/22/2020

--- js/tut64.js
+++ js/tut64.js
@@ -1,6 +1,16 @@
 // NodeJs Modules (Built in Modules)
 const fs = require("fs");
 
-const text = fs.readFileSync("dele.txt", "utf-8");
+let text = fs.readFileSync("dele.txt", "utf-8");
+text = text.replace("node js", "NodeJs");
+
+console.log("This is node js file: ");
 console.log(text);
- 
+
+// creating a new file
+console.log("Creating a new file.. ")
+
+fs.writeFileSync("montu.txt", text);
+fs.writeFileSync("demo.txt");
+
+
