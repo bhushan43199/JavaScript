e23e2a648b6c6576901d09b1abbd99f099fe0d2d
montu patel <bhushanketanbhai.patel@calbaptist.edu>
2020-03-01 17:52:43 -0800

    3/1/2020 npm packages

--- js/tut69/tut69.js
+++ js/tut69/tut69.js
@@ -1,4 +1,4 @@
-console.log("This is tutorial 69");
+console.log("This is tutorial 69.0");
 
 // what is NPM 
 // npm is packege maneger of node.js
@@ -9,6 +9,7 @@ console.log("This is tutorial 69");
 // packeges is collection of files that is written by someone else and we are using that in our project.
 // if mistakenly we delete node_modules that just type npm install
 // specific version installation "npm install slugify@1.3.5" in that mejor, minor and patch contains 1.3.5
-
-
-
+// "npm install nodemon --save-dev" put as dev depenancy for production 
+// to install nodemon on globally so you can use anytime in computer "npm install nodemon --global"
+// nodemon use when you change your file and that diectly shows on your browser by that you dont have to start your server again and agian
+// to install any package in npm you can also type "npm i browserify"
