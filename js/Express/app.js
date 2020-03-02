189f00ae2f61335ec197a2f2e33d2b6bb4c46aed
montu patel <bhushanketanbhai.patel@calbaptist.edu>
2020-03-01 20:03:00 -0800

    3/1/2020 express

---
+++ js/Express/app.js
@@ -0,0 +1,6 @@
+// this is tutorial of express
+// what is express
+// It is web application framework package 
+// to know is use of express refer tut67 , in that we use if else, express automatically do everything for you
+
+
--- js/tut69/tut69.js
+++ js/tut69/tut69.js
@@ -1,15 +1,15 @@
 console.log("This is tutorial 69.0");
 
 // what is NPM 
-// npm is packege maneger of node.js
-// that directory become packege and everything will be added in dependency in packege.json
+// npm is package manager of node.js
+// that directory become package and everything will be added in dependency in package.json
 // npm install slugify
 // npm install express
 // node module folder contains all modules related to project
-// packeges is collection of files that is written by someone else and we are using that in our project.
+// packages is collection of files that is written by someone else and we are using that in our project.
 // if mistakenly we delete node_modules that just type npm install
 // specific version installation "npm install slugify@1.3.5" in that mejor, minor and patch contains 1.3.5
-// "npm install nodemon --save-dev" put as dev depenancy for production 
+// "npm install nodemon --save-dev" put as dev dependency for production 
 // to install nodemon on globally so you can use anytime in computer "npm install nodemon --global"
 // nodemon use when you change your file and that diectly shows on your browser by that you dont have to start your server again and agian
 // to install any package in npm you can also type "npm i browserify"
